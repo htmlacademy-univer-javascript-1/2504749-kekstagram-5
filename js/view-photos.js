b4bcb7b5a93650bce96b96c56688f703f4b67702
Камилла <Kamilla.Gil@urfu.me>
2024-11-30 01:28:31 +0500

    30.11 изменения

--- js/view-photos.js
+++ js/view-photos.js
@@ -6,6 +6,7 @@ const pictureTemplate = document.querySelector('#picture').content.querySelector
 const pictures = createPhotos();
 const picturesListFragment = document.createDocumentFragment();
 
+
 pictures.forEach(({url,description,likes,comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   const pictureImage = pictureElement.querySelector('.picture_img');
@@ -15,6 +16,7 @@ pictures.forEach(({url,description,likes,comments}) => {
   pictureElement.querySelector('.picture_comments').textContent = comments.length;
   picturesListFragment.appendChild(pictureElement);
 });
+
 
 picturesListElement.appendChild(picturesListFragment);
 
