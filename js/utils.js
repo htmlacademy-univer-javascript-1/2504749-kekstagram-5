75ccf1b33c492736bb74c4f302f0dbb9aec315e8
Камилла <Kamilla.Gil@urfu.me>
2024-12-05 23:27:32 +0500

    7. Манипуляции с DOM 05/12

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,7 @@
-function getRandomInt(min, max) {
+
+// Функция для генерации случаного числа в заданном диапазоне
+function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export { getRandomInt };
+export { getRandomInteger };
