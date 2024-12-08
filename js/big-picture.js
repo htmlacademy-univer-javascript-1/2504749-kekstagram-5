394b983de68566197e424630cecc2ad5fa701b84
Камилла <Kamilla.Gil@urfu.me>
2024-12-09 00:33:09 +0500

    финал 8.1

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,4 +1,4 @@
-import { isEscapeKey } from './util.js';
+import { isEscapeKey } from './utils.mjs';
 
 const bigPicture = document.querySelector('.big-picture');
 const commentsList = bigPicture.querySelector('.social__comments');
